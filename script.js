f1671ed38fb8bfe59f1ac563b1e0a59d9d347fc8
NoanWasTaken <delatouchenoan@gmail.com>
2025-01-17 17:34:01 +0100

    more powa to civic

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-
 var speed = 100;
 var model = "golf gti";
-console.log("The car A is a"+model+", and goes to "+speed+" km/h !!!");
+console.log("The car A is a" + model + ", and goes to " + speed + " km/h !!!");
+
+var speed = 250;
+var model = "honda civic";
+console.log("The car B is a" + model + ", and goes to " + speed + " km/h !!!");
